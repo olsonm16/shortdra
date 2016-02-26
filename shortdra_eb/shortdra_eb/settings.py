1efc1f6fb3a7fffe4c6dfb1634e0c28e184a428a
olsonm16 <olsonm16@mail.wlu.edu>
2016-02-25 22:35:46 -0500

    pls

--- shortdra_eb/shortdra_eb/settings.py
+++ shortdra_eb/shortdra_eb/settings.py
@@ -29,11 +29,6 @@ ALLOWED_HOSTS = []
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
-if 'RDS_DB_NAME' not in os.environ:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"), '/var/www/static', 
-    ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 STATIC_URL = '/static/'
 
