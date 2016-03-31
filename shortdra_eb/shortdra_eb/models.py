c9b5e284cf663af65262feb6c874d5b536175c8f
olsonm16 <olsonm16@mail.wlu.edu>
2016-03-31 15:11:25 -0400

    base

--- shortdra_eb/shortdra_eb/models.py
+++ shortdra_eb/shortdra_eb/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 class GermanyStory(models.Model):
 	title = models.CharField(max_length=128, unique=True)
-	slug = models.SlugField(max_length=100, unique=True)
+	#slug = models.SlugField(max_length=100, unique=True)
 	posted = models.DateField(db_index=True, auto_now_add=True)
 	
 	@permalink
